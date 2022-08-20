0ab13a33986031f1eacbd3a505b3b613238404e3
mai-sakai <cbp.vbbs-sntm0930@outlook.jp>
2022-08-20 23:19:14 +0900

    修正

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -11,15 +11,14 @@
 
         <!-- Styles -->
         <style>
-            /* html, body {
+            html, body {
                 background-color: #fff;
                 color: #636b6f;
                 font-family: 'Nunito', sans-serif;
                 font-weight: 200;
                 height: 100vh;
                 margin: 0;
-            } */
-a
+            }
             .full-height {
                 height: 100vh;
             }
